60167309912dd380885f5a687b78b53b4cbd8669
John Hui <790714073@qq.com>
2017-05-21 03:18:15 +0800

    new Logistic.py test_Logistic.py

--- SynXNLP/classify/KNN.py
+++ SynXNLP/classify/KNN.py
@@ -11,10 +11,10 @@ import operator
 
 class KNN(object):
     def __init__(self):
-        self.unknown = 0 # 未知数据的向量 (1xN)
-        self.train_data = 0 # M个已知数据的向量 (NxM)
-        self.labels = 0 # M个已知数据的标签的向量 (1xM)
-        self.k = 5 # 选择前k个最相似的数据 (k一般是不大于20的整数)
+        self._unknown = 0 # 未知数据的向量 (1xN)
+        self._train_data = 0 # M个已知数据的向量 (NxM)
+        self._labels = 0 # M个已知数据的标签的向量 (1xM)
+        self._k = 5 # 选择前k个最相似的数据 (k一般是不大于20的整数)
 
     def predict(self, unknown:list, existingdata:list, labels:list, k = 5):
         """
@@ -24,23 +24,23 @@ class KNN(object):
                 k: 选择前k个最相似的数据 (k一般是不大于20的整数)
         Output: 算法判断出的未知数据的标签
         """
-        self.unknown = np.array(unknown)
-        self.train_data = np.array(existingdata)
-        self.labels = np.array(labels)
-        self.k = k
+        self._unknown = np.array(unknown)
+        self._train_data = np.array(existingdata)
+        self._labels = np.array(labels)
+        self._k = k
 
-        data_set_size = self.train_data.shape[0] # 获得已知数据M的大小
+        data_set_size = self._train_data.shape[0] # 获得已知数据M的大小
 
         # 用tile函数使unknown扩展为NxM的矩阵，并与已知数据做减法
-        diff_mat = np.tile(self.unknown, (data_set_size,1)) - self.train_data
+        diff_mat = np.tile(self._unknown, (data_set_size,1)) - self._train_data
         sq_diff_mat = diff_mat**2 # 平方
         sq_distances = sq_diff_mat.sum(axis=1) # axis=0，表示列，axis=1，表示行。
         distances = sq_distances**0.5 # 开方
         indices = distances.argsort() # 返回从小到大的值的索引
 
         label_count = {} # 创建计数字典
-        for i in range(self.k): # 统计距离最小的前k个标签分别的个数
-            label = self.labels[indices[i]]
+        for i in range(self._k): # 统计距离最小的前k个标签分别的个数
+            label = self._labels[indices[i]]
             label_count[label] = label_count.get(label, 0) + 1
         sorted_label_count = sorted(label_count.items(), key = operator.itemgetter(1), reverse = True)
         # sorted函数返回一个由一个tuple组成的list，如[(0, 3)]
---
+++ SynXNLP/classify/Logistic.py
@@ -0,0 +1,58 @@
+#!user/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Created on 20170518
+@author: JohnHuiWB
+'''
+
+import numpy as np
+import random
+
+class Logistic(object):
+    def __init__(self):
+        self._weights = [] # 所求权值
+
+
+
+    def train(self, train_data:list, labels:list, iter = 150, empirical_parameter = 0.01):
+        """
+        采用随机梯度上升法，进行logistic回归
+        Input:  train_data 要求为标准的数值型数据集
+                labels 数据集对应的类别
+                iter 对整个数据集的迭代次数，默认为200次
+                empirical_parameter 经验参数，默认为0.01
+        Output: 
+        """
+        train_data = np.mat(train_data) # 转换为 数据个数*自变量个数 的矩阵
+        labels = np.mat(labels).T # 转换为列矩阵，便于计算
+        m, n = np.shape(train_data)
+        self._weights = np.ones((n, 1))
+        print("Building model......")
+        for j in range(iter):
+            data_indx = list(range(m))
+            for i in range(m):
+                # 使alpha随着迭代次数不断减小
+                alpha = 4 / (1.0 + i + j) + empirical_parameter 
+                rand_indx = random.choice(data_indx) # 随机选取更新
+
+                h = self._sigmiod(np.sum(train_data[rand_indx] * self._weights))
+                error = labels[rand_indx] - h;
+                self._weights += alpha * train_data[rand_indx].T * error
+                data_indx.remove(rand_indx)
+        print('Finish')
+
+        return self._weights
+
+    def _sigmiod(self, x):
+        return 1.0 / (1 + np.exp(-x))
+
+    def train_result(self):
+        return self._weights
+
+    def predict(self, data:list):
+        data = np.mat(data)
+        prob = self._sigmiod(np.sum(data * self._weights))
+
+        if prob > 0.5: return 1
+        else: return 0
